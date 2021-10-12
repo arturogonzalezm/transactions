d491ee2ee2279a4bef4feceac5492509bb588593
arturogonzalezm <arturo@arturosolutions.com.au>
2021-10-13 00:26:04 +1100

    Re-format report.py

--- etl/report.py
+++ etl/report.py
@@ -8,7 +8,10 @@ gold_df = spark.read.json(OUTPUT_PATH + 'gold/*.json', schema=gold_schema)
 
 gold_df.createOrReplaceTempView("top_agents")
 top_agents_query = """
-                    SELECT AgentID, PostCode, max(Amount) AS MaxAmount FROM top_agents GROUP BY AgentID, PostCode ORDER BY MaxAmount DESC;
+                    SELECT AgentID, PostCode, max(Amount) AS MaxAmount 
+                    FROM top_agents 
+                    GROUP BY AgentID, PostCode 
+                    ORDER BY MaxAmount DESC;
                    """
 top_agents_df = spark.sql(top_agents_query)
 top_agents_df.show(200, truncate=False)
